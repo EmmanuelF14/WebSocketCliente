eabef48b3e3e28868d5057bf8914ee55335dd295
Emman <Emman@EMMANUEL>
2025-06-06 00:40:25 -0600

    Agregar archivos de proyecto.

---
+++ WebSocketCliente/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using WebSocketSharp;
+
+namespace WebSocketCliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ws = new WebSocket("ws://localhost:8181"))
+            {
+                ws.OnOpen += (sender, e) =>
+                {
+                    Console.WriteLine("Conectado al servidor WebSocket.");
+                    ws.Send("¡Hola desde el cliente C#!");
+                };
+
+                ws.OnMessage += (sender, e) =>
+                {
+                    Console.WriteLine("Mensaje del servidor: " + e.Data);
+                };
+
+                ws.OnClose += (sender, e) =>
+                {
+                    Console.WriteLine("Conexión cerrada.");
+                };
+
+                ws.Connect();
+
+                // Permitir enviar mensajes manuales
+                string input;
+                Console.WriteLine("Escribe un mensaje para enviar al servidor. Escribe 'salir' para terminar.");
+
+                while ((input = Console.ReadLine()) != "salir")
+                {
+                    ws.Send(input);
+                }
+
+                ws.Close();
+            }
+        }
+    }
+}
---
+++ WebSocketCliente/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("WebSocketCliente")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("WebSocketCliente")]
+[assembly: AssemblyCopyright("Copyright © HP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("d5b00c6c-cc63-40d0-bce8-7809daa8b0a6")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
